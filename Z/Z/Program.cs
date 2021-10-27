6897901046959382820353ab8f558a799541565a
B3Xray <skymonk251@mail.ru>
2021-10-27 21:13:33 +0400

    Izmenenie coda dlya dvuh uzerov

--- Z/Z/Program.cs
+++ Z/Z/Program.cs
@@ -6,7 +6,7 @@ namespace Z
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Изменение кода для двух участников в одном файле!");
         }
     }
 }
